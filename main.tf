26f6c945c68c1f59555118b1866b869797d2ab47
Swapnil Hedau <swapnilhedau@hotmail.com>
2022-10-18 11:12:52 +0530

    aws_vpc

--- main.tf
+++ main.tf
@@ -9,7 +9,17 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region                  = "ap-south-1"
-  shared_credentials_file = "~/.aws/credentials"
-  profile                 = "swapnilhedau"
+  region                   = "ap-south-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscode"
+}
+
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev-vpc"
+  }
 }
